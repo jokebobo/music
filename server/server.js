b33ef34d81c3a0c13d8c589694e90f4c62b6f8f3
jokerbobo33 <chenjia9393@163.com>
2020-07-20 20:24:25 +0800

    thrid commit

--- server/server.js
+++ server/server.js
@@ -18,38 +18,43 @@ app.all('*', function (req, res, next) {
   next();
 });
 app.use((req, res, next) => {
-  res.header('access-control-allow-origin', 'http://localhost:8000')
+  res.header('access-control-allow-origin', 'http://localhost:8000', 'http://127.0.0.1:8080', 'http://zt.house365.com')
 
   res.header('Access-Control-Allow-Credentials', true);//
 
   next();
 })
+
 //跨域访问数据cors设置 
 app.use(cors({
-  origin: ['http://localhost:8000'],//允许这个域的访问
+  origin: ['http://localhost:8000', 'http://127.0.0.1:8080'],//允许这个域的访问
   methods: ['GET', 'POST'],
   alloweHeaders: ['Conten-Type', 'Authorization']//只允许带这两种请求头的访问
 }))
+console.log("zh is sb");
+console.log("zh is  sb");
 
 app.get('/test', function (req, res) {
   res.send("返回数据233");
 })
 app.get('/music', async function (req, res) {
   console.log("获取传递参数", req.query);
-  if(req.query.id){
+  if (req.query.id) {
     // 如果存在id 则返回对应数据
     res.send(await musiclist.find().where({ id: req.query.id }));
   }
-  if(req.query.all){
+  if (req.query.all) {
     // 不存在返回所有数据
     res.send(await musiclist.find());
   }
-  
+
 })
 app.post('/test1', function (req, res) {
   console.log("获取参数", req.body);
   res.send(req.body);
 })
+
+
 // 监听服务端口
 app.listen('2500', function () {
   console.log("server start!");
